0b55de5a01249f6b0c0208f6c3d3005230275e5b
Mohamed Moeen <mohamed.moeen166@gmail.com>
2025-10-12 01:52:37 +0300

    adding base classes and directories

---
+++ MovieRecommendationSystem/src/main/java/repositories/MovieRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+}
